8add77ff4f86a631d698ed9a5dce99ee2735217f
unknown <agabekyan.e@milandr.ru>
2022-07-19 22:59:32 +0500

    исвользуем неверное выражение, тесты падают, в том числе и CI

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,51 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldPostToPostman() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("some value"))
+        ;
+    }
+
+    @Test
+    void shouldReturnCurrency() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("currency:USD")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("currency:RUB"));
+    }
+
+    @Test
+    void shouldReturnId() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("id:2")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("id:3"));
+    }
+}
